5a572cab26ee162c763640d67b8567ae3d676d3c
01Qin <sisukasqin@hotmail.com>
2025-11-21 22:01:28 +0200

    Add macro definition for SW_2

--- Qin-v1/main.h
+++ Qin-v1/main.h
@@ -9,9 +9,10 @@
 #define WRAP_VALUE 999
 #define PWM_MAX 1000
 
-#define SW_1 8 // middle button - light and dispenser switch
-#define SW_0 9 // left button - led switch
-#define BUTTONS_SIZE 2
+#define SW_1 8 // middle button - Power off/on
+#define SW_0 9 // left button - dispense pills
+#define SW_2 7 // right button - starts motor, calib, LED
+#define BUTTONS_SIZE 3
 
 #define LED_D1 22 // right LED
 #define LEDS_SIZE 1
@@ -24,7 +25,6 @@
 #define INS_SIZE 4
 
 #define SENSOR_PIN 28 // Optical sensor input with pull-up
-#define INPUT_LENGTH 200 // Maximum input line length
 #define SAFE_MAX_STEPS 20480 // Safety limit: 5 * 4096 steps
 #define STEP_DELAY_MS 2
 
