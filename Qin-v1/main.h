82e3a4bb4f4c8692f3500e71043da5d08fb21f7b
01Qin <sisukasqin@hotmail.com>
2025-11-22 18:16:43 +0200

    Add macro definition for dispense and blink intervals

--- Qin-v1/main.h
+++ Qin-v1/main.h
@@ -27,7 +27,9 @@
 #define SENSOR_PIN 28 // Optical sensor input with pull-up
 #define SAFE_MAX_STEPS 20480 // Safety limit: 5 * 4096 steps
 #define STEP_DELAY_MS 2
-#define LED_DELAY_MS 5
+#define LED_DELAY_MS 5 // no longer use, may delete later
+#define DISPENSE_INTERVAL_MS 30000 // 30 seconds
+#define BLINK_INTERVAL_MS 250 // Blink every 250ms
 
 // Type of event coming from the interrupt callback
 typedef enum {
@@ -65,15 +67,22 @@ const uint8_t sequence[8][4] = {
 
 void init_buttons(const uint *buttons);
 void leds_initialisation(const uint *leds);
-void blink_led (uint led_pin, uint button_pin, int delay_ms);
+
 void set_brightness(const uint *leds, uint brightness);
+void start_blink_led (const uint led_pin);
+void stop_blink_led (const uint led_pin);
+
 uint clamp_to_wrap(int bright_value);
 
-void ini_coil_pins(const uint *coil_pins); // Initialize motor coil output pins as outputs
-void ini_sensor(); // Initialize optical sensor input with internal pull-up
+void init_coil_pins(const uint *coil_pins); // Initialize motor coil output pins as outputs
+void init_sensor(); // Initialize optical sensor input with internal pull-up
 int do_calibration(const uint *coil_pins, const uint8_t sequence[8][4], int max, int revolution_steps[3]); // Measure steps per revolution using the optical sensor
 void step_motor(const uint *coil_pins, int step, const uint8_t sequence[8][4]); // Perform one half-step of the stepper motor
-void run_motor(const uint *coil_pins, const uint8_t sequence[8][4], int count, int steps_per_rev); // Run the motor for N * (1/8) revolutions using the calibrated steps per revolution
+void run_motor(const uint *coil_pins, const uint8_t sequence[8][4], int count, int steps_to_move); // Run the motor for N * (1/8) revolutions using the calibrated steps per revolution
 
+// --- ISR and Callback Prototypes ---
+void gpio_callback(uint gpio, uint32_t events);
+bool blink_timer_callback(struct repeating_timer *t);
+bool dispense_timer_callback(struct repeating_timer *t);
 
 #endif //QIN_V1_MAIN_H
